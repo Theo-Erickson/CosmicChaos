3f3127c09cbcc21918ec6969fd1b1d7a748ea28a
ThePatrickLauderdale <40611013+ThePatrickLauderdale@users.noreply.github.com>
2018-11-09 12:03:04 -0800

    Fixed PhaseEnemy to work with Aggrospace, added enemies to level 1

---
+++ Assets/Scripts/AggroSpace.cs
@@ -0,0 +1,31 @@
+﻿/*AggroSpace.cs
+ * Script for the AggroSpace of an enemy
+ * Scriot should be applied on an empty object with a
+ * large collider taking up an entire room/zone
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AggroSpace : MonoBehaviour{
+    public GameObject player;
+    public bool playerInside = false;
+
+    void Start() {
+        if (player == null) { player = GameObject.Find("Player"); }
+    }
+
+    private void OnTriggerStay(Collider other) {
+        if (other.tag == "Player") {
+            playerInside = true;
+            Debug.Log("player entered");
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if(other.tag == "Player") {
+            playerInside = false;
+        }
+    }
+}
--- Assets/Scripts/PhaseEnemy.cs
+++ Assets/Scripts/PhaseEnemy.cs
@@ -7,31 +7,46 @@ public class PhaseEnemy : MonoBehaviour {
     public int world;
     public int defaultWorld;
 
+    public GameObject aggroSpace;
+
     [Header("Phased Enemy Properties")]
     public bool returnWithPlayer;
     [Tooltip(" > 2 is suggested")]public float maxReturnDistance;
+    [Tooltip(" < 2 is suggested")] public float damageDistance;
     public GameObject player;
 
     private Player playerScript;
+    private bool playerInAggroSpace;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		if(player == null) { player = GameObject.Find("Player");}
         playerScript = player.GetComponent<Player>();
     }
 	
 	// Update is called once per frame
 	void Update () {
-        print(Vector3.Distance(this.transform.position, player.transform.position));
-        //hunt player in the same world
-        if (world == player.GetComponent<Player>().currentWorld && active) {
-            this.transform.position = Vector3.MoveTowards(this.transform.position, player.transform.position, 0.01f);
-        }
+        playerInAggroSpace = aggroSpace.GetComponent<AggroSpace>().playerInside;
+        if (playerInAggroSpace)
+        {
+            print(Vector3.Distance(this.transform.position, player.transform.position));
+            //hunt player in the same world
+            if (world == player.GetComponent<Player>().currentWorld && active)
+            {
+                this.transform.position = Vector3.MoveTowards(this.transform.position, player.transform.position, 0.01f);
+            }
 
-        //go along with player if they are close enough
-        if (Input.GetKeyDown(KeyCode.LeftShift) && playerScript.canShift && Vector3.Distance(this.transform.position, player.transform.position) < maxReturnDistance) {
-            swapWorld();
-        } 
+            //go along with player if they are close enough
+            if (Input.GetKeyDown(KeyCode.LeftShift) && playerScript.canShift && Vector3.Distance(this.transform.position, player.transform.position) < maxReturnDistance && returnWithPlayer)
+            {
+                swapWorld();
+            }
+            //do damage if very close to the player
+            if(Vector3.Distance(this.transform.position, player.transform.position) < damageDistance)
+            {
+                doDamage();
+            }
+        }
 	}
 
 
@@ -39,5 +54,10 @@ public class PhaseEnemy : MonoBehaviour {
         if (world == 1) { world = 2; } else { world = 1; }
         if (world == 1) this.transform.parent = playerScript.world1.transform;
         if (world == 2) this.transform.parent = playerScript.world2.transform;
+    }
+
+    public void doDamage()
+    {
+        print("feel sanity dropping");
     }
 }

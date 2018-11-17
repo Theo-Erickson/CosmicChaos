0da5830e05bbfeabd237f0fb2eefeec5eab7bf86
Alfredo Cuevas <cuevasalfredo0@gmail.com>
2018-11-17 14:39:42 -0800

    created an audio log prefab. added the script that enables the sound to play when clicked

--- Assets/Scripts/PlayAudioLog.cs
+++ Assets/Scripts/PlayAudioLog.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayAudioLog : MonoBehaviour {
+    // This script takes an Audio Clip, plays it, and then destroys he object.
+    // The script is only played when the interact button is pressed while moused over
 
-    [SerializeField] private KeyCode interactButton;
-    [SerializeField] private AudioClip audioLog;
-    [SerializeField] private Transform playerTransform;
-    [SerializeField] private float playerDistFromSwitch;
+
+    [SerializeField] private KeyCode interactButton;     // Button to be pressed
+    [SerializeField] private AudioClip audioLog;         // Audio Clip
+    [SerializeField] private Transform playerTransform;  // Player Reference
+    [SerializeField] private float playerDistFromSwitch; // Distance player can interact with object
 
     private bool playedAudioLog;
 
@@ -18,16 +21,23 @@ public class PlayAudioLog : MonoBehaviour {
 
     private void OnMouseOver()
     {
-        if(Input.GetKey(interactButton) && (Vector3.Distance(playerTransform.position, transform.position) <= playerDistFromSwitch))
+        PlayAudioLogWhenSelected();
+    }
+
+    private void PlayAudioLogWhenSelected()
+    {
+        if (Input.GetKey(interactButton) && (Vector3.Distance(playerTransform.position, transform.position) <= playerDistFromSwitch))
         {
             if (!playedAudioLog)
             {
                 print(audioLog.name);
                 AudioScript.instance.playSFX(audioLog.name);
 
+                Destroy(this.gameObject);
+
                 playedAudioLog = true;
             }
-            
+
         }
     }
 }
